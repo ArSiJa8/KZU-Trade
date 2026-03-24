a746118f8ff6a87912a8bc5f6c4acc846491ae02
phise2e-hub <phise2e@gmail.com>
2026-03-24 14:53:28 +0100

    Create AboutMe.vue

---
+++ app/components/AboutMe.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+</script>
+<template>
+  <div style="align-content:center ">
+  <font color=“red“><h1>Über uns</h1></font>
+  <font color="blue"><h2>Phil Sennhauser</h2>
+  <p>14 Jahre alt</p>
+  <p>Wohnort Höri</p>
+  <p>Verantwortlich für:</p>
+  <p>Organisation von Events der Webseite</p>
+  <p>Werbung der Webseite</p>
+  <p>Administratives</p></font>
+  <font color="green"><h2>Silvan Hofstetter</h2>
+  <p>14 Jahre alt</p>
+  <p>Wohnort Bülach</p>
+  <p>Verantwortlich für:</p>
+  <p>Design der Webseite</p>
+  <p>Programmieren der Webseite</p></font>
+  </div>
+</template>
